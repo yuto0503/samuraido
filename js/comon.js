b886d94335320b077f0544d426e141f0e9e3bf8f
‘yuto’_0503 <y.yuto.0503@gmail.comgit config --global user.name yuto_0503git config --global user.email y.yuto.0503@gmail.com>
2022-11-27 11:45:37 +0900

    2回目コミット

--- js/comon.js
+++ js/comon.js
@@ -149,3 +149,10 @@ function moveAnimation(){
       $(window).scroll(function (){
         delayScrollAnime();
       });
+
+  
+  // ローディングアニメーション
+$(window).on('load',function(){
+  $("#splash").delay(1500).fadeOut('slow');
+  $("#splash_logo").delay(1200).fadeOut('slow');
+});
